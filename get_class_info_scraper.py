685309fdbfe4a82ff6324dc80aed7cb86554c4f5
Gabriel Ruiz <ggruiz.ds@gmail.com>
2022-01-13 16:23:39 -0800

    focus on getting info first

--- get_class_info_scraper.py
+++ get_class_info_scraper.py
@@ -1,6 +1,6 @@
 
+import time
 import pandas as pd
-import numpy as np
 import requests
 import bs4
 
@@ -9,25 +9,30 @@ def class_page(id):
     return f'https://catalog.fullerton.edu/preview_course_nopop.php?catoid=70&coid={id}'
 
 
+# Get all the course codes and turn them into URLs
 df = pd.read_csv('codes.csv')
 df['urls'] = df['courseids'].apply(class_page)
 
 
 def get_info_from_page(url):
+    """Get the info from an individual URL"""
     first_res = requests.get(url)
     first_soup = bs4.BeautifulSoup(first_res.text, 'html.parser')
     all_course_info = first_soup.select('#course_preview_title')[0].parent
     return all_course_info
 
 
-test_url = df['urls'][0]
-children = list(filter(lambda child: child.name != 'br',
-                get_info_from_page(test_url).contents))
-
-new_children = np.array(children, dtype='object')
-
-def is_h1(x): return x.name == 'h1'
-
-is_h1_vec = np.vectorize(is_h1)
-bool_arr = is_h1_vec(new_children)
-print(new_children[bool_arr])
+# The scraper loop for every single URL
+# Outputs html file {course code}.html with relevant info
+# Set to scrape in chunks of 200 every 3 minutes
+# So as to not overload the servers
+for index, row in df.iterrows():
+    print(f'remaining: {df.count() - index - 1}')
+    children = get_info_from_page(row['urls'])
+    html_content = str(children.prettify())
+    html_file = open(f'course_data/{row["courseids"]}.html', 'w')
+    html_file.write(html_content)
+    html_file.close()
+    if (index != 0 and index % 200 == 0):
+        print('waiting for next iteration...')
+        time.sleep(180)
